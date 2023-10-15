bc47503093e2817c362223e67fb505e936758546
Weezy-88 <145158306+Weezy-88@users.noreply.github.com>
2023-10-15 16:11:36 +0100

    added link element for farenheit

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ let hour = now.getHours();
 let minutes = now.getMinutes();
 let dateElement = document.querySelector("#date");
 dateElement.innerHTML = `${day} ${hour}:${minutes}`;
+
 function displayTemp(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -24,12 +25,24 @@ function displayTemp(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   let windSpeedElement = document.querySelector("#windSpeed");
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png`
+  );
 }
-iconElement = document.querySelector("#icon");
-iconElement.setAttribute(
-  "src",
-  `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png`
-);
-let apiKey = "b2aa1fad6a010fof20ab443f842te386";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={UAE}}&key=${apiKey}`;
-axios.get(apiUrl).then(displayTemp);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+function search(city) {
+  let apiKey = "b2aa1fad6a010fof20ab443f842te386";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayTemp);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
